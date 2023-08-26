cee43a14bab2cf25763f7e14e3901d9f1cbc5c95
Caio <108128119+DocCaio@users.noreply.github.com>
2023-08-26 11:04:47 -0300

    Finish dark
    Finish dark mode transition

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 const changeThemeBtn = document.querySelector("#change-theme");
 
-changeThemeBtn.addEventListener("change" , function () {
+// Togle dark mode
+function toggleDarkMode() {
     document.body.classList.toggle("dark");
+}
+
+//Light or dark mode 
+
+function loadTheme() {
+    const darkMode = localStorage.getItem("dark");
+
+if(darkMode) {
+    toggleDarkMode();
+}
+
+}
+
+loadTheme();
+
+changeThemeBtn.addEventListener("change", function () {
+    toggleDarkMode();
+
+    // Save or remove dark mode
+    localStorage.removeItem("dark");
+
+
+    if(document.body.classList.contains("dark")) {
+        localStorage.setItem("dark , 1");
+    }
 });
